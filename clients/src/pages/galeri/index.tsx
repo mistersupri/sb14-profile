e946a2bda32f3be49a6d5b7469f15e7c36b825d5
Ahmad Supriyanto <supri.contact@gmail.com>
2025-10-08 15:12:21 +0700

    feat: polish gallery page

--- clients/src/pages/galeri/index.tsx
+++ clients/src/pages/galeri/index.tsx
@@ -1,7 +1,75 @@
-import { MainLayout } from "@/layouts";
+import { BigTitleLayout } from "@/layouts";
+import { Image } from "antd";
+import classNames from "classnames";
+
+const random = Date.now();
 
 const GaleriPage = () => {
-  return <MainLayout></MainLayout>;
+  const imageList = [
+    {
+      url: `https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png?${random}`,
+    },
+    {
+      url: `https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png?${random}`,
+    },
+    {
+      url: `https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png?${random}`,
+    },
+    {
+      url: `https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png?${random}`,
+    },
+    {
+      url: `https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png?${random}`,
+    },
+    {
+      url: `https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png?${random}`,
+    },
+  ];
+
+  return (
+    <BigTitleLayout title="Galeri">
+      <div className="xl:max-w-380 m-auto pb-32 px-4 md:px-8 lg:px-32 flex gap-8">
+        <div className="flex-1 grid grid-cols-3 gap-2 flex-wrap">
+          {imageList.map((item) => (
+            <Image
+              className="object-cover rounded-xl"
+              height={164}
+              width="100%"
+              src={item.url}
+              placeholder={
+                <Image
+                  className="object-cover rounded-xl"
+                  height={164}
+                  width="100%"
+                  preview={false}
+                  src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png?x-oss-process=image/blur,r_50,s_50/quality,q_1/resize,m_mfit,h_200,w_200"
+                />
+              }
+            />
+          ))}
+        </div>
+        <div className="flex flex-col gap-4 w-2/7">
+          <h2 className="text-xl font-bold">Label</h2>
+          <div className="flex flex-wrap gap-2">
+            <a
+              className={classNames([
+                "rounded bg-gray-200 py-2 px-4 font-bold",
+                "bg-teal-800 text-white",
+              ])}
+            >
+              SEMUA
+            </a>
+            <a className="rounded bg-gray-200 py-2 px-4 font-bold hover:bg-black hover:text-white cursor-pointer">
+              #ANBK
+            </a>
+            <a className="rounded bg-gray-200 py-2 px-4 font-bold hover:bg-black hover:text-white cursor-pointer">
+              #KJP
+            </a>
+          </div>
+        </div>
+      </div>
+    </BigTitleLayout>
+  );
 };
 
 export default GaleriPage;
