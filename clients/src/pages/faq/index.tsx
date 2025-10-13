00387f200362cac6097d9afdccfd9705d8de0fc1
Ahmad Supriyanto <supri.contact@gmail.com>
2025-10-13 08:49:24 +0700

    feat: integrate faq service

--- clients/src/pages/faq/index.tsx
+++ clients/src/pages/faq/index.tsx
@@ -1,40 +1,36 @@
 import { BigTitleLayout } from "@/layouts";
 import { Collapse } from "antd";
 import { ChevronRight } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import { Services } from "@/services";
+import { useMemo } from "react";
 
 const FAQPage = () => {
-  const faqList = [
-    {
-      key: "1",
-      label: "This is default size panel header",
-      children:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at felis ullamcorper, vulputate massa eu, porttitor metus. Vestibulum sagittis egestas vulputate. Mauris id nibh sit amet justo vulputate dictum. Phasellus eleifend enim sollicitudin odio pulvinar ullamcorper. Sed tempus vitae erat eget mollis. Suspendisse imperdiet suscipit dolor, at cursus elit imperdiet at. Etiam imperdiet turpis sed velit ornare sollicitudin. Curabitur eu lacinia orci, non auctor sem.",
-    },
-    {
-      key: "2",
-      label: "This is default size panel header",
-      children:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at felis ullamcorper, vulputate massa eu, porttitor metus. Vestibulum sagittis egestas vulputate. Mauris id nibh sit amet justo vulputate dictum. Phasellus eleifend enim sollicitudin odio pulvinar ullamcorper. Sed tempus vitae erat eget mollis. Suspendisse imperdiet suscipit dolor, at cursus elit imperdiet at. Etiam imperdiet turpis sed velit ornare sollicitudin. Curabitur eu lacinia orci, non auctor sem.",
-    },
-    {
-      key: "3",
-      label: "This is default size panel header",
-      children:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at felis ullamcorper, vulputate massa eu, porttitor metus. Vestibulum sagittis egestas vulputate. Mauris id nibh sit amet justo vulputate dictum. Phasellus eleifend enim sollicitudin odio pulvinar ullamcorper. Sed tempus vitae erat eget mollis. Suspendisse imperdiet suscipit dolor, at cursus elit imperdiet at. Etiam imperdiet turpis sed velit ornare sollicitudin. Curabitur eu lacinia orci, non auctor sem.",
-    },
-    {
-      key: "4",
-      label: "This is default size panel header",
-      children:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at felis ullamcorper, vulputate massa eu, porttitor metus. Vestibulum sagittis egestas vulputate. Mauris id nibh sit amet justo vulputate dictum. Phasellus eleifend enim sollicitudin odio pulvinar ullamcorper. Sed tempus vitae erat eget mollis. Suspendisse imperdiet suscipit dolor, at cursus elit imperdiet at. Etiam imperdiet turpis sed velit ornare sollicitudin. Curabitur eu lacinia orci, non auctor sem.",
-    },
-  ];
+  const { data: faqData } = useQuery({
+    queryKey: ["FAQ_LIST"],
+    queryFn: () => Services.getFaqList(),
+  });
+
+  const faqList = useMemo(
+    () =>
+      faqData?.data?.map((item: Record<string, any>) => ({
+        key: item.id,
+        label: item.question,
+        children: item.answer || "-",
+      })) || [],
+    [faqData]
+  );
+
+  if (!faqData || !faqList.length) return;
 
   return (
     <BigTitleLayout title="FAQ">
       <div className="xl:max-w-380 m-auto pb-32 px-4 md:px-8 lg:px-32 flex flex-col gap-4">
-        {faqList.map((item) => (
-          <div className="[&_.ant-collapse]:rounded-2xl! [&_.ant-collapse]:bg-transparent! [&_.ant-collapse-content]:bg-transparent! [&_.ant-collapse-header]:font-bold [&_.ant-collapse-content]:rounded-b-2xl!">
+        {faqList.map((item: any) => (
+          <div
+            key={item.key}
+            className="[&_.ant-collapse]:rounded-2xl! [&_.ant-collapse]:bg-transparent! [&_.ant-collapse-content]:bg-transparent! [&_.ant-collapse-header]:font-bold [&_.ant-collapse-content]:rounded-b-2xl!"
+          >
             <Collapse
               expandIcon={({ isActive }) => (
                 <ChevronRight style={{ rotate: isActive ? "90deg" : "0deg" }} />
